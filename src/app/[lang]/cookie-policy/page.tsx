8050705a5abd3b624e56c731fd9b3265c919d98b
Geneviève Perron Migneron <info@gen-migneron.com>
2026-05-01 14:16:57 -0400

    Change button for back button

--- src/app/[lang]/cookie-policy/page.tsx
+++ src/app/[lang]/cookie-policy/page.tsx
@@ -1,24 +1,31 @@
 import Link from 'next/link'
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 import { getDictionary } from '@/lib/dictionary'
-import { Locale } from '../../../../i18n-config'
+import { Locale, i18n } from '../../../../i18n-config';
 
 export default async function CookiePolicyPage({ params: { lang } }: { params: { lang: Locale } }) {
   const { cookiePolicy } = await getDictionary(lang)
 
   return (
-    <main className="policy-page">
-      <h1 className="intro-title text-xl md:text-xl font-bold mb-2"><span>_</span>{cookiePolicy.title}</h1>
-      <p className="text-sm mb-2">{cookiePolicy.updated}</p>
-      <p className="text-sm mb-2">{cookiePolicy.intro}</p>
-      <ul className="ml-2">
-        {cookiePolicy.cookies.map(item => (
-          <li className="text-sm mb-2" key={item}>{item}</li>
-        ))}
-      </ul>
-      <p className="text-sm mb-2">{cookiePolicy.rights}</p>
-      <Link className="button" href={lang === 'en' ? '/' : `/${lang}`}>
-        <span>{cookiePolicy.home}</span>
+    <>
+      <Link
+        className="p-2 fixed z-10 top-0 left-0 flex items-center text-lg md:text-2xl text-white"
+        key={lang}
+        href={lang === i18n.defaultLocale ? "/" : `/${lang}`}
+      >
+        <FaLongArrowAltLeft className="mr-2" />Back
       </Link>
-    </main>
+      <main className="policy-page">
+        <h1 className="intro-title text-xl md:text-xl font-bold mb-2"><span>_</span>{cookiePolicy.title}</h1>
+        <p className="text-sm mb-2">{cookiePolicy.updated}</p>
+        <p className="text-sm mb-2">{cookiePolicy.intro}</p>
+        <ul className="ml-2">
+          {cookiePolicy.cookies.map(item => (
+            <li className="text-sm mb-2" key={item}>{item}</li>
+          ))}
+        </ul>
+        <p className="text-sm mb-2">{cookiePolicy.rights}</p>
+      </main>
+    </>
   )
 }
--- src/app/[lang]/privacy-policy/page.tsx
+++ src/app/[lang]/privacy-policy/page.tsx
@@ -1,25 +1,32 @@
 import Link from 'next/link'
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 import { getDictionary } from '@/lib/dictionary'
-import { Locale } from '../../../../i18n-config'
+import { Locale, i18n } from '../../../../i18n-config';
 
 export default async function PrivacyPolicyPage({ params: { lang } }: { params: { lang: Locale } }) {
   const { privacyPolicy } = await getDictionary(lang)
 
   return (
-    <main className="policy-page">
-      <h1 className="intro-title md:text-xl font-bold mb-2"><span>_</span>{privacyPolicy.title}</h1>
-      <p className="text-sm mb-2">{privacyPolicy.updated}</p>
-      <p className="text-xs mb-2">{privacyPolicy.intro}</p>
-      {privacyPolicy.sections.map(section => (
-        <section key={section.title}>
-          <h2 className="text-md font-bold">{section.title}</h2>
-          <p className="text-xs mb-2">{section.text}</p>
-        </section>
-      ))}
-      <p className="d-block text-xs mt-4 mb-4">{privacyPolicy.contact}</p>
-      <Link className="button" href={lang === 'en' ? '/' : `/${lang}`}>
-        <span>{privacyPolicy.home}</span>
+    <>
+      <Link
+        className="p-2 fixed z-10 top-0 left-0 flex items-center text-lg md:text-2xl text-white"
+        key={lang}
+        href={lang === i18n.defaultLocale ? "/" : `/${lang}`}
+      >
+        <FaLongArrowAltLeft className="mr-2" />Back
       </Link>
-    </main>
+      <main className="policy-page">
+        <h1 className="intro-title md:text-xl font-bold mb-2"><span>_</span>{privacyPolicy.title}</h1>
+        <p className="text-sm mb-2">{privacyPolicy.updated}</p>
+        <p className="text-xs mb-2">{privacyPolicy.intro}</p>
+        {privacyPolicy.sections.map(section => (
+          <section key={section.title}>
+            <h2 className="text-md font-bold">{section.title}</h2>
+            <p className="text-xs mb-2">{section.text}</p>
+          </section>
+        ))}
+        <p className="d-block text-xs mt-4 mb-4">{privacyPolicy.contact}</p>
+      </main>
+    </>
   )
 }
